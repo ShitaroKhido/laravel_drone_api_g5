3ca67b179cd4935777b43b68fe5a138b3a55943c
Sreypok-D <sreypokdoem@gmail.com>
2023-05-25 20:34:57 +0700

    [controller] : update function in plan controller

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -52,32 +52,56 @@ class PlanController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Plan $plan)
+    public function show(string $id)
     {
-        //
+        //show plan by id 
+        $plans = Plan::find($id);
+        return response()->json(['message' => 'Show drone on id : ' . $id, 'data' => $plans], 200);
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Plan $plan)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Plan $plan)
+    public function update(Request $request,  $id)
     {
-        //
+    //update plan by id
+         //validation
+         $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|max:30',
+                'assigned_datetime' => 'required',
+                'drone_id' => 'required',
+                'instruction_id' => 'required',
+                'farm_id' => 'required',
+                'user_id' => 'required',
+            ]
+        );
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+        $plans = Plan::where('id',$id)->update([
+            'name'=>$request['name'],
+            'assigned_datetime'=>$request['assigned_datetime'],
+            'drone_id'=>$request['drone_id'],
+            'instruction_id'=>$request['instruction_id'],
+            'farm_id'=>$request['farm_id'],
+            'user_id'=>$request['user_id'],
+        ]);
+        return response()->json(['message' => 'update plan successfully !' , 'data' => $plans],200);
+
+        
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Plan $plan)
+    public function destroy(string $id)
     {
-        //
+        // delete plan by id
+        $plans = Plan::destroy($id);
+        return response()->json(['message' => 'Delete successfully', 'data' =>$plans],200);
     }
 }

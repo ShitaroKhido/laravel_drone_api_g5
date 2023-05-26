e5f06b86f6cecb5173a3fe1b77cd51f65f1efef1
ShitaroKhido <khidpagna01@gmail.com>
2023-05-26 07:35:19 +0700

    [route]: updated api route.

--- routes/api.php
+++ routes/api.php
@@ -12,15 +12,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('drones', DroneController::class);
-Route::resource('map_pictures', MapPictureController::class);
-Route::resource('plans', PlanController::class);
-Route::get('/drones/{id}/location', [DroneController::class, 'getDroneLocation']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    // Get Map:
+    Route::resource('/map_pictures', MapPictureController::class);
+    // Get Plan:
+    Route::resource('/plans', PlanController::class);
     // Get drone location with ID:
     Route::get('/drone/{id}/location', [DroneController::class, 'getDroneLocation']);
     // Drone resources:

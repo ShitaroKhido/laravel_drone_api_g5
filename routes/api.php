a3cf2c663cd515accb4b0a1f7dfe01da6ff8f6c5
ShitaroKhido <khidpagna01@gmail.com>
2023-05-27 17:23:04 +0700

    [route]: edited route.

--- routes/api.php
+++ routes/api.php
@@ -14,23 +14,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/upload', [DroneController::class, 'sendImage']);
-
-// update  instrct drone by id 
-Route::put('/drones/{id}', [DroneController::class, 'instructDroneById']);
-// Instruction 
-Route::get('/instruction/{id}', [InstructionController::class, 'getDroneInstruction']);
-// plan name 
-Route::get('/plan/{name}', [PlanController::class, 'reqestPlanName']);
 
 
-
-Route::get('/download/{location}/{id}', [DroneController::class, 'downloadImg']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    // Uploading image:
+    Route::post('/upload', [DroneController::class, 'sendImage']);
+    // Update  drone instruct drone by id: 
+    Route::put('instruction/drones/{id}', [DroneController::class, 'instructDroneById']);
+    // Get drone instruction:
+    Route::get('/instruction/{id}', [InstructionController::class, 'getDroneInstruction']);
+    // plan name :
+    Route::get('/plan/{name}', [PlanController::class, 'reqestPlanName']);
+    // Download maps:
+    Route::get('/maps/{location}/{id}', [DroneController::class, 'downloadImg']);
     // Get Map:
     Route::resource('/map_pictures', MapPictureController::class);
     // Get Plan:
